2f70545dd807a022d02eb9fb62f0c87d43e238a7
Vicente Bercasio <vicentetobio00@gmail.com>
2026-08-02 10:41:35 +0800

    chore: update app/checkout.tsx

--- app/checkout.tsx
+++ app/checkout.tsx
@@ -167,7 +167,7 @@ export default function CheckoutScreen() {
                 <View className="mb-lg">
                   <Text
                     accessibilityRole="header"
-                    className="font-serif text-[38px] leading-[46px] text-neutral-1000"
+                    className="font-montserrat-bold text-display tracking-[-0.8px] text-neutral-1000"
                   >
                     Checkout
                   </Text>
